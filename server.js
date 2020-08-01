cc63baaba16acd77781cbb3985a400abefee4856
t.sarker <tanmay.sarker@baruchmail.cuny.edu>
2020-08-01 19:37:37 -0400

    updated

--- server.js
+++ server.js
@@ -1,64 +1,22 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const {
-    v4: uuidv4
-} = require('uuid');
-const fs = require('fs');
-const util = require('util');
+  // DEPENDECIES =======================
+  const express = require("express");
 
-const PORT = process.env.PORT || 3000;
+  // To initialze the app
+  const app = express();
+  const PORT = process.env.PORT || 3000;
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAysnc = util.promisify(fs.writeFile);
-
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({
+  // Sets up the Express app to handle data parsing
+  app.use(express.urlencoded({
     extended: true
-}));
+  }));
+  app.use(express.json());
+  app.use(express.static("./public"));
 
-//serve page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+  // require route file
+  require("./routes/apiroutes")(app);
+  require("./routes/htmlroutes")(app);
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
-
-//api
-app.get('/api/notes', (req, res) => {
-    readFileAsync('./db/db.json', 'utf8')
-        .then((data) => {
-            const notes = JSON.parse(data);
-            res.json(notes);
-        })
-        .catch((err) => console.error(err));
-});
-
-app.post('/api/notes', async (req, res) => {
-    let notes = await readFileAsync('./db/db.json', 'utf8');
-    notes = JSON.parse(notes);
-    req.body.id = uuidv4();
-    notes.push(req.body);
-
-    await writeFileAysnc('./db/db.json', JSON.stringify(notes));
-    res.json(notes[notes.length - 1]);
-});
-
-app.delete('/api/notes/:id', async (req, res) => {
-    const id = req.params.id;
-
-    let notes = await readFileAsync('./db/db.json', 'utf8');
-    notes = JSON.parse(notes);
-
-    notes = notes.filter((note) => note.id !== id);
-
-    await writeFileAysnc('./db/db.json', JSON.stringify(notes));
-    res.json(notes);
-});
-
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-});
+  // Start the server to begin listening
+  app.listen(PORT, function () {
+    console.log("App listening on PORT" + PORT);
+  });
